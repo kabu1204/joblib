319b9c5bcdce7003bea7a24e8b757133926d0ad8
kabu1204 <201930344367@mail.scut.edu.cn>
2021-11-05 20:21:39 +0800

    add sub-project 'test' for testing

---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by yuchengye on 2021/11/5.
+//
+
+#include "main.h"
+
+int main(){
+    hello();
+}
---
+++ test/main.h
@@ -0,0 +1,10 @@
+//
+// Created by yuchengye on 2021/11/5.
+//
+
+#ifndef JOBLIB_MAIN_H
+#define JOBLIB_MAIN_H
+
+#include "library.h"
+
+#endif //JOBLIB_MAIN_H
