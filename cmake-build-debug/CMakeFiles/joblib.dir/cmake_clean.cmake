ea173ab6f09f67ca575f07f2c62bbe43eb0e3acf
kabu1204 <201930344367@mail.scut.edu.cn>
2021-11-05 14:25:51 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/joblib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joblib.dir/library.cpp.o"
+  "libjoblib.dylib"
+  "libjoblib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joblib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef JOBLIB_LIBRARY_H
+#define JOBLIB_LIBRARY_H
+
+void hello();
+
+#endif //JOBLIB_LIBRARY_H
