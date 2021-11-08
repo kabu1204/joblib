566a85375f02ddab79a6db7fe14f79274f25a8fe
kabu1204 <1036058484@qq.com>
2021-11-08 20:36:48 +0800

    Revert "rename inputTypeImpl to STLtypeImpl"
    This reverts commit 0628edaa07deafb9d0100879c145b383842e58d5.

--- include/Serializer/Serializer.hpp
+++ include/Serializer/Serializer.hpp
@@ -46,7 +46,7 @@ output_type(Serializer& buf, T& value) {
     output_type_impl(buf, value);
 }
 
-#include "STLTypeImpl.hpp"
+#include "InputTypeImpl.hpp"
 
 template<typename T>
 inline Serializer& Serializer::operator<<(T const& value) {
